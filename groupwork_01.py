4c904f41d4d405d03d89a5639336093e90c25f16
KJLuebeck <Kathi-Jaeger@t-online.de>
2022-05-16 15:24:02 +0200

    Groupwork unfinished

---
+++ groupwork_01.py
@@ -0,0 +1,63 @@
+no_1=float(input("Erste Zahl (Float)"))
+no_2=float(input("ZweiteZahl (Float)"))
+no_3=float(input("Dritte Zahl (Float)"))
+no_4=float(input("Vierte Zahl (Float)"))
+no_5=float(input("Fuenfte Zahl (Int)"))
+no_6=int(input("Sechste Zahl (Int)"))
+no_7=int(input("Siebte Zahl (Int)"))
+no_8=int(input("Achte Zahl (Int)"))
+no_9=int(input("Neunte Zahl (Int)"))
+no_10=int(input("Zehnte Zahl (Int)"))
+
+#+
+plus1=no_1+no_2
+print (f"{no_1} + {no_2} = {plus1}")
+
+plus2=no_2+no_3
+print (f"{no_2} + {no_3} = {plus2}")
+
+plus3=no_4+no_5
+print (f"{no_4} + {no_5} = {plus3}")
+
+plus4=no_6+no_7
+print (f"{no_6} + {no_7} = {plus4}")
+
+plus5=no_7+no_8
+print (f"{no_7} + {no_8} = {plus5}")
+
+# **
+power1 = int (no_4)**int (no_2)
+print (f"{no_4} ** {no_2} = {power1}!")
+
+power2 = int (no_5)**int (no_3)
+print (f"{no_5} ** {no_3} = {power2}!")
+
+power3 = int (no_10)**int (no_2)
+print (f"{no_10} ** {no_2} = {power3}!")
+
+power4 = int (no_5)** int (no_3)
+print (f"{no_5} ** {no_3} = {power4}!")
+
+power5 = int (no_3)**int (no_6)
+print (f"{no_3} ** {no_6} = {power5}!")
+
+# %
+rem1 = (no_3)
+rem1%= (no_2)
+print (f"{no_3} % {no_2} = {rem1}")
+
+rem2 = (no_1)
+rem2%= (no_10)
+print (f"{no_1} % {no_10} = {rem2}")
+
+rem3 = (no_3)
+rem3%= (no_8)
+print (f"{no_3} % {no_8} = {rem3}")
+
+rem4 = (no_10)
+rem4%= (no_8)
+print (f"{no_10} % {no_8} = {rem4}")
+
+rem5 = (no_6)
+rem5%= (no_8)
+print (f"{no_6} % {no_8} = {rem5}")
